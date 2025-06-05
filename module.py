e4d31a12b2f8449e26cbef53d5f5371faa6959da
Leroy Anders <leroy.wagner20@gmail.com>
2025-06-05 20:15:54 +0300

    Fix NLLB src_lang handling and add example

--- module.py
+++ module.py
@@ -7,25 +7,74 @@ import torch
 
 # Mapping from ISO 639-1 codes to NLLB-200 language codes
 LANG_CODE_MAP: Dict[str, str] = {
-    'af': 'afr_Latn', 'sq': 'sqi_Latn', 'am': 'amh_Ethi', 'ar': 'arb_Arab',
-    'hy': 'hye_Armn', 'az': 'azj_Latn', 'be': 'bel_Cyrl', 'bn': 'ben_Beng',
-    'bs': 'bos_Latn', 'bg': 'bul_Cyrl', 'ca': 'cat_Latn', 'zh': 'zho_Hans',
-    'zh-cn': 'zho_Hans', 'zh-tw': 'zho_Hant', 'hr': 'hrv_Latn', 'cs': 'ces_Latn',
-    'da': 'dan_Latn', 'nl': 'nld_Latn', 'en': 'eng_Latn', 'et': 'est_Latn',
-    'fi': 'fin_Latn', 'fr': 'fra_Latn', 'ka': 'kat_Geor', 'de': 'deu_Latn',
-    'el': 'ell_Grek', 'gu': 'guj_Gujr', 'ht': 'hat_Latn', 'ha': 'hau_Latn',
-    'he': 'heb_Hebr', 'hi': 'hin_Deva', 'hu': 'hun_Latn', 'is': 'isl_Latn',
-    'id': 'ind_Latn', 'it': 'ita_Latn', 'ja': 'jpn_Jpan', 'kn': 'kan_Knda',
-    'kk': 'kaz_Cyrl', 'ko': 'kor_Hang', 'lv': 'lvs_Latn', 'lt': 'lit_Latn',
-    'mk': 'mkd_Cyrl', 'ms': 'zsm_Latn', 'mr': 'mar_Deva', 'ne': 'npi_Deva',
-    'no': 'nob_Latn', 'fa': 'pes_Arab', 'pl': 'pol_Latn', 'pt': 'por_Latn',
-    'pa': 'pan_Guru', 'ro': 'ron_Latn', 'ru': 'rus_Cyrl', 'sr': 'srp_Cyrl',
-    'sk': 'slk_Latn', 'sl': 'slv_Latn', 'es': 'spa_Latn', 'sw': 'swh_Latn',
-    'sv': 'swe_Latn', 'ta': 'tam_Taml', 'te': 'tel_Telu', 'th': 'tha_Thai',
-    'tr': 'tur_Latn', 'uk': 'ukr_Cyrl', 'ur': 'urd_Arab', 'vi': 'vie_Latn'
+    "af": "afr_Latn",
+    "sq": "sqi_Latn",
+    "am": "amh_Ethi",
+    "ar": "arb_Arab",
+    "hy": "hye_Armn",
+    "az": "azj_Latn",
+    "be": "bel_Cyrl",
+    "bn": "ben_Beng",
+    "bs": "bos_Latn",
+    "bg": "bul_Cyrl",
+    "ca": "cat_Latn",
+    "zh": "zho_Hans",
+    "zh-cn": "zho_Hans",
+    "zh-tw": "zho_Hant",
+    "hr": "hrv_Latn",
+    "cs": "ces_Latn",
+    "da": "dan_Latn",
+    "nl": "nld_Latn",
+    "en": "eng_Latn",
+    "et": "est_Latn",
+    "fi": "fin_Latn",
+    "fr": "fra_Latn",
+    "ka": "kat_Geor",
+    "de": "deu_Latn",
+    "el": "ell_Grek",
+    "gu": "guj_Gujr",
+    "ht": "hat_Latn",
+    "ha": "hau_Latn",
+    "he": "heb_Hebr",
+    "hi": "hin_Deva",
+    "hu": "hun_Latn",
+    "is": "isl_Latn",
+    "id": "ind_Latn",
+    "it": "ita_Latn",
+    "ja": "jpn_Jpan",
+    "kn": "kan_Knda",
+    "kk": "kaz_Cyrl",
+    "ko": "kor_Hang",
+    "lv": "lvs_Latn",
+    "lt": "lit_Latn",
+    "mk": "mkd_Cyrl",
+    "ms": "zsm_Latn",
+    "mr": "mar_Deva",
+    "ne": "npi_Deva",
+    "no": "nob_Latn",
+    "fa": "pes_Arab",
+    "pl": "pol_Latn",
+    "pt": "por_Latn",
+    "pa": "pan_Guru",
+    "ro": "ron_Latn",
+    "ru": "rus_Cyrl",
+    "sr": "srp_Cyrl",
+    "sk": "slk_Latn",
+    "sl": "slv_Latn",
+    "es": "spa_Latn",
+    "sw": "swh_Latn",
+    "sv": "swe_Latn",
+    "ta": "tam_Taml",
+    "te": "tel_Telu",
+    "th": "tha_Thai",
+    "tr": "tur_Latn",
+    "uk": "ukr_Cyrl",
+    "ur": "urd_Arab",
+    "vi": "vie_Latn",
 }
 
 MODEL_NAME = "facebook/nllb-200-distilled-600M"
+
 
 class UnlimitedTranslator:
     """Translate large texts using the NLLB neural model."""
@@ -35,6 +84,7 @@ class UnlimitedTranslator:
         self.src = self._resolve_lang(src or detect(text))
         self.dest = self._resolve_lang(dest)
         self.tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+        self.tokenizer.src_lang = self.src
         self.model = AutoModelForSeq2SeqLM.from_pretrained(MODEL_NAME)
         self.translated_text = self._translate_text()
 
@@ -47,8 +97,7 @@ class UnlimitedTranslator:
         with torch.no_grad():
             generated_tokens = self.model.generate(
                 **inputs,
-                forced_bos_token_id=self.tokenizer.convert_tokens_to_ids(self.dest),
-                src_lang=self.src,
+                forced_bos_token_id=self.tokenizer.lang_code_to_id[self.dest],
                 max_length=1024,
             )
         return self.tokenizer.decode(generated_tokens[0], skip_special_tokens=True)
